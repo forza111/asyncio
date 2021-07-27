004dce5a2b69d32bb5571c1287f83631e4b119c5
forza111 <nikitos.ionkin@yandex.ru>
2021-07-27 17:44:31 +0300

    Презентация Дэвида Бизли Rev.01

---
+++ DavidBeazleyACuriousCourseonCoroutinesandConcurrency/Part1/follow.py
@@ -0,0 +1,25 @@
+import time
+
+# A generator that follows a log file like Unix 'tail -f'.
+
+# Note: To see this example work, you need to apply to
+# an active server log file.  Run the program "logsim.py"
+# in the background to simulate such a file.  This program
+# will write entries to a file "access-log".
+
+
+
+def follow(thefile):
+    thefile.seek(0,2)
+    while True:
+        line = thefile.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        yield line
+
+if __name__ == "__main__":
+    logfile = open("access-log")
+
+    for line in follow(logfile):
+        print(line,)
